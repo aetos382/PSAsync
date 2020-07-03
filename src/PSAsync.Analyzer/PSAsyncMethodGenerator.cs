ab4ae676dce6d738914a82db925e39541646de4d
aetos <aetos@aerie.jp>
2020-07-04 05:23:42 +0900

    add generator

---
+++ src/PSAsync.Analyzer/PSAsyncMethodGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace PSAsync.Analyzer
+{
+    [Generator]
+    public class PSAsyncMethodGenerator :
+        ISourceGenerator
+    {
+        public void Initialize(
+            InitializationContext context)
+        {
+        }
+
+        public void Execute(
+            SourceGeneratorContext context)
+        {
+        }
+    }
+}
