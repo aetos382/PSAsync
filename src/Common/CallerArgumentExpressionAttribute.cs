65af78a9d18cd3070c4844bd68a0b23eaf53a497
aetos <aetos@aerie.jp>
2020-07-04 13:46:55 +0900

    CallerArgumentExpressionAttribute を internal にする
    public だと .NET Core プロジェクトで標準のものと衝突してしまう

--- src/Common/CallerArgumentExpressionAttribute.cs
+++ src/Common/CallerArgumentExpressionAttribute.cs
@@ -1,13 +1,20 @@
+/*
+    PSAsync.Analyzer は .NET Standard 2.0 だが、CallerArgumentExpressionAttribute は .NET Standard には存在しないので、
+    コードの互換性のためにダミークラスを入れておく。
+*/
+
 #if NETSTANDARD2_0
 
 namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-    public sealed class CallerArgumentExpressionAttribute : Attribute
+    internal sealed class CallerArgumentExpressionAttribute :
+        Attribute
     {
-        public CallerArgumentExpressionAttribute(string parameterName)
+        public CallerArgumentExpressionAttribute(
+            string parameterName)
         {
-            ParameterName = parameterName;
+            this.ParameterName = parameterName;
         }
 
         public string ParameterName { get; }
