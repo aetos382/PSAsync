1bee3fa4879a717a22d7d89aaaf539c9dc9b5fd0
aetos <aetos@aerie.jp>
2020-07-07 15:24:13 +0900

    AsyncCmdlet / AsyncPSCmdlet を追加
    使ってもいいし、使わなくてもいい

---
+++ src/PSAsync/AsyncPSCmdlet.cs
@@ -0,0 +1,56 @@
+﻿using System.Management.Automation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PSAsync
+{
+    public abstract class AsyncPSCmdlet :
+        PSCmdlet,
+        IAsyncCmdlet
+    {
+        protected AsyncPSCmdlet()
+        {
+        }
+
+        protected override void BeginProcessing()
+        {
+            this.DoBeginProcessingAsync();
+        }
+
+        protected override void ProcessRecord()
+        {
+            this.DoProcessRecordAsync();
+        }
+
+        protected override void EndProcessing()
+        {
+            this.DoEndProcessingAsync();
+        }
+
+        protected override void StopProcessing()
+        {
+            this.CancelAsyncOperation();
+        }
+
+        [SkipImplementationCheck]
+        public virtual Task BeginProcessingAsync(
+            CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        [SkipImplementationCheck]
+        public virtual Task ProcessRecordAsync(
+            CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+        
+        [SkipImplementationCheck]
+        public virtual Task EndProcessingAsync(
+            CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
