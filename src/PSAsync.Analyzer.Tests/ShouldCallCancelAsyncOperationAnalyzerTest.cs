e6615b41e854a8771766d8078b25e0bc3fac23b4
aetos <aetos@aerie.jp>
2020-07-09 04:54:28 +0900

    iroiro

---
+++ src/PSAsync.Analyzer.Tests/ShouldCallCancelAsyncOperationAnalyzerTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using Xunit;
+
+using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<PSAsync.Analyzer.ShouldCallCancelAsyncOperationsAnalyzer>;
+
+namespace PSAsync.Analyzer.Tests
+{
+    public class ShouldCallCancelAsyncOperationAnalyzerTest
+    {
+        [Fact]
+        public async Task Test()
+        {
+            const string source = @"
+using System;
+using System.Management.Automation;
+
+namespace Hoge
+{
+    [Cmdlet(VerbsDiagnostics.Test, ""Hoge"")]
+    public class TestCmdlet : Cmdlet, IAsyncCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            this.WriteObject(1);
+        }
+    }
+}";
+
+            await Verifier.VerifyAnalyzerAsync(source);
+        }
+    }
+}
---
+++ src/PSAsync.Analyzer/ShouldCallCancelAsyncOperationsAnalyzer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Immutable;
+
+using Microsoft;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace PSAsync.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ShouldCallCancelAsyncOperationsAnalyzer :
+        DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Hoge";
+
+        private static readonly DiagnosticDescriptor _descriptor = new DiagnosticDescriptor(
+            DiagnosticId,
+            "Hoge",
+            "Hoge",
+            "Hoge",
+            DiagnosticSeverity.Info,
+            true);
+
+        private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
+            ImmutableArray.Create(_descriptor);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return _supportedDiagnostics;
+            }
+        }
+
+        public override void Initialize(
+            AnalysisContext context)
+        {
+            Requires.NotNull(context, nameof(context));
+
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationStartAction(
+                this.CompilationStartAction);
+
+            context.RegisterSyntaxNodeAction(
+                this.SyntaxNodeAction,
+                SyntaxKind.ClassDeclaration);
+        }
+
+        private void CompilationStartAction(
+            CompilationStartAnalysisContext context)
+        {
+        }
+
+        private void SyntaxNodeAction(
+            SyntaxNodeAnalysisContext context)
+        {
+            var cancellationToken = context.CancellationToken;
+
+            var syntax = (ClassDeclarationSyntax) context.Node;
+            var symbol = context.SemanticModel.GetDeclaredSymbol(syntax, cancellationToken);
+            var type = context.SemanticModel.GetTypeInfo(syntax, cancellationToken);
+
+            var attributes = symbol.GetAttributes();
+        }
+    }
+}
